176d2177eec3bb4a50487477e824892d9cd595b2
Hossam Samir <hossamsamir@outlook.com>
2025-06-13 16:29:13 +0300

    main application layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import * as THREE from 'three';
+import { Canvas } from './components/canvas/canvas';
+import { ControlUi } from './components/control-ui/control-ui';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Canvas, ControlUi],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -12,37 +14,37 @@ export class App implements AfterViewInit {
   protected title = '3d_web';
 
   ngAfterViewInit(): void {
-    // This method is called after the view has been initialized
+    // // This method is called after the view has been initialized
 
-    const width = window.innerWidth, height = window.innerHeight;
+    // const width = window.innerWidth, height = window.innerHeight;
 
-    // init
+    // // init
 
-    const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
-    camera.position.z = 1;
+    // const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
+    // camera.position.z = 1;
 
-    const scene = new THREE.Scene();
+    // const scene = new THREE.Scene();
 
-    const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-    const material = new THREE.MeshNormalMaterial();
+    // const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    // const material = new THREE.MeshNormalMaterial();
 
-    const mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+    // const mesh = new THREE.Mesh(geometry, material);
+    // scene.add(mesh);
 
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(width, height);
-    renderer.setAnimationLoop(animate);
-    document.body.appendChild(renderer.domElement);
+    // const renderer = new THREE.WebGLRenderer({ antialias: true });
+    // renderer.setSize(width, height);
+    // renderer.setAnimationLoop(animate);
+    // document.body.appendChild(renderer.domElement);
 
-    // animation
+    // // animation
 
-    function animate(time: number) {
+    // function animate(time: number) {
 
-      mesh.rotation.x = time / 2000;
-      mesh.rotation.y = time / 1000;
+    //   mesh.rotation.x = time / 2000;
+    //   mesh.rotation.y = time / 1000;
 
-      renderer.render(scene, camera);
+    //   renderer.render(scene, camera);
 
-    }
+    // }
   }
 }
--- src/app/components/canvas/canvas.ts
+++ src/app/components/canvas/canvas.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-canvas',
@@ -6,6 +7,42 @@ import { Component } from '@angular/core';
   templateUrl: './canvas.html',
   styleUrl: './canvas.css'
 })
-export class Canvas {
+export class Canvas implements AfterViewInit {
 
+ constructor() {}
+
+    ngAfterViewInit(): void {
+      // This method is called after the view has been initialized
+  
+      const width = window.innerWidth, height = window.innerHeight;
+  
+      // init
+  
+      const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
+      camera.position.z = 1;
+  
+      const scene = new THREE.Scene();
+  
+      const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+      const material = new THREE.MeshNormalMaterial();
+  
+      const mesh = new THREE.Mesh(geometry, material);
+      scene.add(mesh);
+  
+      const renderer = new THREE.WebGLRenderer({ antialias: true });
+      renderer.setSize(width, height);
+      renderer.setAnimationLoop(animate);
+      document.body.appendChild(renderer.domElement);
+  
+      // animation
+  
+      function animate(time: number) {
+  
+        mesh.rotation.x = time / 2000;
+        mesh.rotation.y = time / 1000;
+  
+        renderer.render(scene, camera);
+  
+      }
+    }
 }
