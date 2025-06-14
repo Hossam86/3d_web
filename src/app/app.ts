c09c10ce1f086c0ac94192418412a4cca45507ec
Hossam Samir <hossamsamir@outlook.com>
2025-06-14 03:12:41 +0300

    update app layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,41 +10,5 @@ import { ControlUi } from './components/control-ui/control-ui';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements AfterViewInit {
-  protected title = '3d_web';
-
-  ngAfterViewInit(): void {
-    // // This method is called after the view has been initialized
-
-    // const width = window.innerWidth, height = window.innerHeight;
-
-    // // init
-
-    // const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
-    // camera.position.z = 1;
-
-    // const scene = new THREE.Scene();
-
-    // const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-    // const material = new THREE.MeshNormalMaterial();
-
-    // const mesh = new THREE.Mesh(geometry, material);
-    // scene.add(mesh);
-
-    // const renderer = new THREE.WebGLRenderer({ antialias: true });
-    // renderer.setSize(width, height);
-    // renderer.setAnimationLoop(animate);
-    // document.body.appendChild(renderer.domElement);
-
-    // // animation
-
-    // function animate(time: number) {
-
-    //   mesh.rotation.x = time / 2000;
-    //   mesh.rotation.y = time / 1000;
-
-    //   renderer.render(scene, camera);
-
-    // }
-  }
+export class App {
 }
